3bee31fa5d31d6a7d768ae736e291f74bd30fa61
AsadbekNurmamatov2002 <nurmamatovasadbek85.gmail.com>
2024-06-14 02:19:23 +0500

    my project

--- app/users/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile
-
-@receiver(post_save, sender=User)  # Connect to the built-in post_save signal
-def update_quantity_on_order_confirmation(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
